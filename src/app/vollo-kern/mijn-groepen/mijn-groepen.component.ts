d9e6d0b7adcb06aca8c5af480b6fed79af46ef87
Danny Lagrouw <danny.lagrouw@gmail.com>
2018-07-20 16:48:48 +0200

    VOLLO-16 Styling Mijn groepen

--- src/app/vollo-kern/mijn-groepen/mijn-groepen.component.ts
+++ src/app/vollo-kern/mijn-groepen/mijn-groepen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MijnGroepenService } from './mijn-groepen.service';
 import { AgGridNg2 } from 'ag-grid-angular';
+import { GridOptions, RowClickedEvent, ColDef } from 'ag-grid';
 
 @Component({
   selector: 'vollo-mijn-groepen',
@@ -10,9 +11,18 @@ import { AgGridNg2 } from 'ag-grid-angular';
 export class MijnGroepenComponent implements OnInit {
   @ViewChild('agGrid') agGrid: AgGridNg2;
 
-  columnDefs = [
-    { headerName: 'School', field: 'school.naam' },
-    { headerName: 'Groep', field: 'naam' }
+  gridOptions = <GridOptions>{
+    enableColResize: true,
+    enableSorting: true,
+    enableFilter: true,
+    onRowClicked: (event: RowClickedEvent) => {
+      console.log('groep id', event.data.id);
+    }
+  };
+  columnDefs = <ColDef[]>[
+    { headerName: 'ID', field: 'id', hide: true },
+    { headerName: 'School', field: 'school.naam', width: 300 },
+    { headerName: 'Groep', field: 'naam', width: 200 }
   ];
   rowData: any;
 
