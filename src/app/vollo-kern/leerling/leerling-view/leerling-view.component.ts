0cff94aef8cc3204b0cd20f06080f251be98f04e
Danny Lagrouw <danny.lagrouw@gmail.com>
2018-12-24 23:25:27 +0100

    Leerlinganimatie

--- src/app/vollo-kern/leerling/leerling-view/leerling-view.component.ts
+++ src/app/vollo-kern/leerling/leerling-view/leerling-view.component.ts
@@ -2,11 +2,23 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Leerling } from '../../groep/leerling.model';
 import { Score } from '../../common/model/score.model';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'vollo-leerling-view',
   templateUrl: './leerling-view.component.html',
-  styleUrls: ['./leerling-view.component.scss']
+  styleUrls: ['./leerling-view.component.scss'],
+  animations: [
+    trigger('leerling', [
+      transition(
+        '* => *',
+        animate(
+          '300ms',
+          keyframes([style({ opacity: 0, offset: 0 }), style({ opacity: 1, offset: 1 })])
+        )
+      )
+    ])
+  ]
 })
 export class LeerlingViewComponent {
   fotoData: SafeResourceUrl;
